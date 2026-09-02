0e7a4f923c957f5a5a1c3408a058bbfedef2af3d
TautCony <i@tautcony.xyz>
2026-09-02 18:50:09 +0800

    refactor: improve ClpiClipInfo data processing and add helper methods for readability

--- src/ChapterTool.Core/Importing/Disc/Clpi/ClpiClipInfo.cs
+++ src/ChapterTool.Core/Importing/Disc/Clpi/ClpiClipInfo.cs
@@ -25,9 +25,24 @@ internal sealed record ClpiClipInfo(
         var numberOfSourcePackets = container.ReadUInt32BigEndian();
         container.SkipBytes(128);
 
-        ClpiTsTypeInfo? tsTypeInfo = null;
-        var atcDeltas = new List<ClpiAtcDelta>();
-        var fonts = new List<ClpiFontInfo>();
+        var tsTypeInfo = ReadTsTypeInfo(container);
+        var atcDeltas = ReadAtcDeltas(container, isCC5);
+        var fonts = ReadFonts(container, applicationType);
+        container.Complete("clip info");
+        return new ClpiClipInfo(
+            length,
+            clipStreamType,
+            applicationType,
+            tsRecordingRate,
+            numberOfSourcePackets,
+            isCC5,
+            tsTypeInfo,
+            atcDeltas,
+            fonts);
+    }
+
+    private static ClpiTsTypeInfo? ReadTsTypeInfo(MplsBoundedStream container)
+    {
         if (container.Remaining >= 2)
         {
             var tsTypeInfoLength = container.ReadUInt16BigEndian();
@@ -46,59 +61,65 @@ internal sealed record ClpiClipInfo(
                 var validity = container.ReadByteChecked();
                 var formatIdentifier = container.ReadAscii(4);
                 container.SkipBytes(tsTypeInfoLength - 5);
-                tsTypeInfo = new ClpiTsTypeInfo(validity, formatIdentifier);
+                return new ClpiTsTypeInfo(validity, formatIdentifier);
             }
         }
 
-        if (isCC5)
+        return null;
+    }
+
+    private static IReadOnlyList<ClpiAtcDelta> ReadAtcDeltas(MplsBoundedStream container, bool isCC5)
+    {
+        var atcDeltas = new List<ClpiAtcDelta>();
+        if (!isCC5)
         {
-            if (container.Remaining < 2)
-            {
-                throw new InvalidDataException("CLPI ATC delta information is truncated.");
-            }
-
-            container.SkipBytes(1);
-            var atcDeltaCount = container.ReadByteChecked();
-            ClpiParseLimits.ValidateCountByBudget(atcDeltaCount, 14, container.Remaining, "ATC delta");
-            for (var i = 0; i < atcDeltaCount; i++)
-            {
-                var delta = container.ReadUInt32BigEndian();
-                var fileId = container.ReadAscii(5);
-                var fileCode = container.ReadAscii(4);
-                container.SkipBytes(1);
-                atcDeltas.Add(new ClpiAtcDelta(delta, fileId, fileCode));
-            }
+            return atcDeltas;
         }
 
-        if (applicationType == 6)
+        if (container.Remaining < 2)
         {
-            if (container.Remaining < 2)
-            {
-                throw new InvalidDataException("CLPI subtitle font information is truncated.");
-            }
-
-            container.SkipBytes(1);
-            var fontCount = container.ReadByteChecked();
-            ClpiParseLimits.ValidateCountByBudget(fontCount, 6, container.Remaining, "subtitle font");
-            for (var i = 0; i < fontCount; i++)
-            {
-                var fileId = container.ReadAscii(5);
-                container.SkipBytes(1);
-                fonts.Add(new ClpiFontInfo(fileId));
-            }
+            throw new InvalidDataException("CLPI ATC delta information is truncated.");
         }
 
-        container.Complete("clip info");
-        return new ClpiClipInfo(
-            length,
-            clipStreamType,
-            applicationType,
-            tsRecordingRate,
-            numberOfSourcePackets,
-            isCC5,
-            tsTypeInfo,
-            atcDeltas,
-            fonts);
+        container.SkipBytes(1);
+        var atcDeltaCount = container.ReadByteChecked();
+        ClpiParseLimits.ValidateCountByBudget(atcDeltaCount, 14, container.Remaining, "ATC delta");
+        for (var i = 0; i < atcDeltaCount; i++)
+        {
+            var delta = container.ReadUInt32BigEndian();
+            var fileId = container.ReadAscii(5);
+            var fileCode = container.ReadAscii(4);
+            container.SkipBytes(1);
+            atcDeltas.Add(new ClpiAtcDelta(delta, fileId, fileCode));
+        }
+
+        return atcDeltas;
+    }
+
+    private static IReadOnlyList<ClpiFontInfo> ReadFonts(MplsBoundedStream container, byte applicationType)
+    {
+        var fonts = new List<ClpiFontInfo>();
+        if (applicationType != 6)
+        {
+            return fonts;
+        }
+
+        if (container.Remaining < 2)
+        {
+            throw new InvalidDataException("CLPI subtitle font information is truncated.");
+        }
+
+        container.SkipBytes(1);
+        var fontCount = container.ReadByteChecked();
+        ClpiParseLimits.ValidateCountByBudget(fontCount, 6, container.Remaining, "subtitle font");
+        for (var i = 0; i < fontCount; i++)
+        {
+            var fileId = container.ReadAscii(5);
+            container.SkipBytes(1);
+            fonts.Add(new ClpiFontInfo(fileId));
+        }
+
+        return fonts;
     }
 
     public bool IsCC5 => IsAtcDelta;
